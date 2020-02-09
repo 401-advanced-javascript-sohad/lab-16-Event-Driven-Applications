91ee3c7fc04a8269b1c25a3400aff2d24622dfa0
sohadQtaitat <qtaitat3@gmail.com>
2020-02-10 01:42:35 +0200

    add test

---
+++ __tests__/app.test.js
@@ -0,0 +1,17 @@
+'use strict ';
+
+// jest.mock('fs');
+
+
+describe('File test', () => {
+  it('can read the file', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+
+
+describe('Event emitting modules', () => {
+  it('tests to come later', () => {
+  });
+});
